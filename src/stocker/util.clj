a4ddb2e72202bb22decc44e0728d71a674233de7
mangosmootie <mangosmoothie@users.noreply.github.com>
2016-04-03 00:27:51 -0500

    adding unzip for downloaded sources

--- src/stocker/util.clj
+++ src/stocker/util.clj
@@ -1,5 +1,10 @@
 (ns stocker.util
-  (:import (java.util Calendar)))
+  (:import (java.util Calendar)
+           (java.util.zip ZipInputStream)
+           (java.io File)
+           (java.io FileOutputStream)
+           (java.io FileInputStream)))
+
 
 (defn market-open? []
   (and (<= 9 (.get (Calendar/getInstance) Calendar/HOUR_OF_DAY) 15)
@@ -13,3 +18,55 @@
     (/ (+ (.get (Calendar/getInstance) Calendar/MINUTE)
           (.get (Calendar/getInstance) Calendar/HOUR_OF_DAY))
      (* 6.5 60))))
+
+(defn strip-extension
+  [filename]
+  (.substring filename 0 (.lastIndexOf filename ".")))
+
+(defn get-files
+  "return all files in given dir path"
+  [dirpath]
+  (let [dir (File. dirpath)]
+    (filter #(.isFile %) (.listFiles dir))))
+
+(defn get-zip-files
+  "return all .zip files in a given dir path"
+  [dirpath]
+  (filter #(.endsWith (.getName %) ".zip") (get-files dirpath)))
+
+(defn zip-entries
+  [zipfile]
+  (lazy-seq
+   (if-let [entry (.getNextEntry zipfile)]
+     (cons entry (zip-entries zipfile)))))
+
+(defn unzip-file
+  "decompress zip file and save to location - preserving filenames"
+  [zipfile outdirpath]
+  (let [buffer (byte-array 1024)
+        outdir (File. (str outdirpath (File/separator) (strip-extension (.getName zipfile))))]
+    (.mkdir outdir)
+    (println "unzipping " (.getPath zipfile))
+    (with-open [zis (ZipInputStream. (FileInputStream. zipfile))]
+      (loop [e (.getNextEntry zis)]
+        (if e
+          (let [filename (.getName e)]
+            (let [outfile (File. (str outdir (File/separator) filename))]
+              (with-open [outstream (FileOutputStream. outfile)]
+                (println "writing file: " (.getPath outfile))
+                (.mkdirs (File. (.getParent outfile)))
+                (loop [len (.read zis buffer)]
+                  (if (< 0 len)
+                    (do
+                      (.write outstream buffer 0 len)
+                      (recur (.read zis buffer)))))))
+            (recur (.getNextEntry zis))))))))
+
+(defn unzip-all
+  "unzip all .zip files in dirpath"
+  [dirpath outdirpath]
+  (doseq [zipfile (get-zip-files dirpath)]
+    (if (.exists (File. (str outdirpath (File/separator) (strip-extension (.getName zipfile)))))
+      (println "skipping file, output dir exists for " (.getPath zipfile))
+      (unzip-file zipfile outdirpath))))
+
